e7239dc15991dfc975dcc405c1f0bf6d810175ae
Robert Gregg <hi@robertgregg.co.uk>
2020-12-17 12:47:38 +0000

    sorted error. Sidebar displaying convoc

--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -1,7 +1,8 @@
 // for storing all of our conversations
 import React, { useContext } from 'react'
 import useLocalStorage from '../hooks/useLocalStorage';
-
+import { useContacts } from './ContactsProvider';
+ 
 const ConversationsContext = React.createContext();
 
 // function to be used in the modal
@@ -12,6 +13,7 @@ export function useConversations() {
 export function ConversationsProvider({ children }) {
     // get the list of conversations
     const [conversations, setConversations ] = useLocalStorage('conversations', []);
+    const { contacts } = useContacts();
 
     // creating a new conversation
     function createConversation(recipients) {
@@ -20,8 +22,25 @@ export function ConversationsProvider({ children }) {
         });
     }
 
+    const formattedConversations = conversations.map(conversation => {
+         const recipients = conversation.recipients.map(recipient => {
+             const contact = contacts.find(contact => {
+                 return contact.id === recipient; // recipient returns an id
+            });
+            const name = (contact && contact.name) || recipient // contact && cont... returns the name of the contact
+            return { id: recipient, name } // if no contact, the name defaults back to the recipient
+         });
+
+         return { ...conversations, recipients } // returns all of the conversations and the newly formatted recipients with a name and ID instead of a list of ids. 
+    });
+
+    const value = {
+        conversations: formattedConversations,
+        createConversation
+    }
+
     return (
-        <ConversationsContext.Provider value={{ conversations, createConversation }}>
+        <ConversationsContext.Provider value={ value }>
             {children}
         </ConversationsContext.Provider> 
     )
